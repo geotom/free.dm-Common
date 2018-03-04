83ec51b4642c5c5197f18ea399ace980fa7eae25
Thomas Wanderer <github@wander.land>
2018-03-04 02:16:31 +0200

    - Updated IPC server class and related util classes. - Added new connection pool for simple IPC server

--- freedm/utils/ipc/connection.py
+++ freedm/utils/ipc/connection.py
@@ -4,18 +4,83 @@ This module defines an IPC server connection object
 '''
 
 # Imports
+import time
+from datetime import timedelta
 from enum import Enum
 from collections import namedtuple
+from typing import Type, TypeVar, List
+
+# free.dm Imports
+from freedm.utils.exceptions import freedmBaseException
 
 
-class ConnectionPool(object):
-#https://www.pythonsheets.com/notes/python-asyncio.html#simple-asyncio-connection-pool
-    pass
+C = TypeVar('C', bound='Connection')
 
 
+class ConnectionPool(set):
+    '''
+    A set of active client sessions
+    '''
+    
+    _max = None
+    @property
+    def max(self):
+        '''Maximum allowed connections'''
+        return self._max
+    @max.setter
+    def max(self, amount: int):
+        if max > len(self):
+            self._max = amount
+        else:
+            raise freedmConnectionPoolMax()
+        
+    def isFull(self) -> bool:
+        '''
+        Returns if this connection pool can still accept new connections
+        '''
+        return False if not self.max else self.max > len(self)
+    
+    def getConnections(self) -> List[Type[C]]:
+        '''
+        Return active connections
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self]
+    
+    def getConnectionsByAddress(self, address: str) -> List[Type[C]]:
+        '''
+        Return active connections from the specified address
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self if c.client_address == address]
+    
+    def getConnectionsByUser(self, uid: int) -> List[Type[C]]:
+        '''
+        Return active connections from the specified user ID
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self if c.uid == uid]
+    
+    def getConnectionsByGroup(self, gid: int) -> List[Type[C]]:
+        '''
+        Return active connections from the specified group ID
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self if c.gid == gid]
+    
+    def getConnectionsByProcess(self, pid: int) -> List[Type[C]]:
+        '''
+        Return active connections from the specified process ID
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self if c.pid == pid]
+
+    def getIdleConnectionsSince(self, period: int) -> List[Type[C]]:
+        '''
+        Return active connections idling longer then the specified period in seconds
+        '''
+        return [c._coro.cr_frame.f_locals['connection'] for c in self if period <= timedelta(seconds=float(time.time() - c.state['update']))]
+                
+                
 class ConnectionType(Enum):
     TEXT_DATA = 1
     STREAM_DATA = 2
+
 
 Connection = namedtuple('Connection', 
     '''
@@ -27,6 +92,13 @@ Connection = namedtuple('Connection',
     server_address
     reader
     writer
-    mode
+    state
     '''
     )
+
+
+class freedmConnectionPoolMax(freedmBaseException):
+    '''
+    Gets thrown when the maximum number of connections cannot be set on the connection pool
+    '''
+    template = 'Cannot set number of maximum connections lower than currently active session'
--- freedm/utils/ipc/server.py
+++ freedm/utils/ipc/server.py
@@ -9,8 +9,9 @@ try:
     import asyncio
     import socket
     import struct
+    import time
     from pathlib import Path
-    from typing import Union, Type, Optional, Set, TypeVar
+    from typing import Union, Type, Optional, Set, TypeVar, List
     
     # free.dm Imports
     from freedm.utils.exceptions import freedmBaseException
@@ -31,24 +32,39 @@ class IPCServer(object):
     A generic server implementation for IPC servers allowing to communicate with connected clients
     '''
     
-    def __init__(self, loop: Optional[Type[asyncio.AbstractEventLoop]]=None, limit: Optional[int]=None, chunksize: Optional[int]=None, mode: Optional[Union[ConnectionType]]=None) -> None:
+    _connectionPool: ConnectionPool=ConnectionPool()
+    
+    def __init__(
+            self,
+            loop: Optional[Type[asyncio.AbstractEventLoop]]=None,
+            limit: Optional[int]=None,
+            chunksize: Optional[int]=None,
+            max_connections: Optional[int]=None,
+            mode: Optional[Union[ConnectionType]]=None
+            ) -> None:
         self.loop = loop or asyncio.get_event_loop()
         self.limit = limit
         self.chunksize = chunksize
         self.mode = mode
+        if isinstance(max_connections, int):
+            self._connectionPool.max = max_connections
         
     async def __aenter__(self) -> I:
         '''A template function that should be implemented by any subclass'''
         return self
         
     async def __aexit__(self, *args) -> None:
-        '''A template function that should be implemented by any subclass'''
+        '''Cancel all pending connections in the connection pool'''
+        for c in self._connectionPool:
+            c.cancel()
         
     async def __await__(self) -> I:
         return await self.__aenter__()
     
     def _getConnection(self, reader: Type[asyncio.StreamReader], writer: Type[asyncio.StreamWriter]) -> Optional[Connection]:
-        '''Builds a connection object based on the info we get from the reader/writer'''
+        '''
+        Builds a connection object based on the info we get from the reader/writer
+        '''
         return Connection(
             socket=writer.get_extra_info('socket'),
             pid=None,
@@ -58,62 +74,91 @@ class IPCServer(object):
             server_address=None,
             reader=reader,
             writer=writer,
-            mode=self.mode or ConnectionType.TEXT_DATA)
+            state={
+                'mode': self.mode or ConnectionType.TEXT_DATA,
+                'creation': time.time(),
+                'update': time.time()
+                }
+            )
     
-    async def _onConnection(self, reader: Type[asyncio.StreamReader], writer: Type[asyncio.StreamWriter]) -> None:
-        '''On new connections read the data and meta info and put the connection to the connection pool'''
-        
+    def _onConnectionRegister(self, reader: Type[asyncio.StreamReader], writer: Type[asyncio.StreamWriter]) -> asyncio.Task:
+        '''
+        Cares for the creation and recycling of connections for any new established session and subsequently hands over the further session handling
+        '''
+        # Check if we can accept?
+        if self._connectionPool.isFull():
+            return
+        # Connection object
         connection = self._getConnection(reader, writer)
-        
-        
-#         def on_connection_register(reader, writer):
-#             coro = on_connection(reader, writer)
-#             fut = asyncio.ensure_future(coro)
-#             connections_made.add(fut)
-#             fut.add_done_callback(lambda res: connections_made.remove(fut))
-#             return fut
-        
-        # TODO: Check if connection already in connection pool?
-        # If yes -> authenticate
-        # If not, proceed
+        # Create asyncio.Task for handling the session
+        session = asyncio.ensure_future(self.handleConnection(connection))
+        self._connectionPool.add(session)
+        session.add_done_callback(lambda task: self._connectionPool.remove(session))
+        return session
+    
+    async def handleConnection(self, connection: Connection) -> None:
+        '''
+        Handle the connection and listen for incoming messages
+        '''
+        # Authenticate
         auth = await self.authenticateConnection(connection)
         if auth:
             print('-> Connection was successfully authenticated')
-            
+        else:
+            return
+        
         # Simple IPC API
-        test = await reader.read(10)
-        if len(test) <= 3:
-            try:
-                code = int(test)
-                print(code)
-                if CommandMessage.supports(code):
-                    await self._handleCommand(CommandMessage.get(code), connection)
-                    return
-            except:
-                pass
+#         header = await reader.read(10)
+#         if len(header) <= 3:
+#             try:
+#                 code = int(header)
+#                 if CommandMessage.supports(code):
+#                     await self._handleCommand(CommandMessage.get(code), connection)
+#                     return
+#             except:
+#                 pass
         
         # Read data depending on the connection type and handle it
-        if connection.mode == ConnectionType.TEXT_DATA:
-            try:
+        if connection.state['mode'] == ConnectionType.TEXT_DATA:
+            print('READING TEXT')
+            while not connection.reader.at_eof():
+                print(1)
                 message = Message(
-                    data=await reader.readline(),
+                    data=await connection.reader.read(self.limit or -1),
                     sender=connection
                     )
-            except Exception as e:
-                message = Message(
-                    data=await reader.read(self.limit or -1),
-                    sender=connection
-                    )
+                print('=DATA', message.data)
+#             try:
+#                 lines = b''
+#                 while not reader.at_eof():
+#                     lines += await reader.readline()
+#                 message = Message(
+#                     data=lines,
+#                     sender=connection
+#                     )
+#             except Exception as e:
+#                 message = Message(
+#                     data=await reader.read(self.limit or -1),
+#                     sender=connection
+#                     )
+            connection.reader.feed_eof()
             await self.handleMessage(message)
-        elif connection.mode == ConnectionType.STREAM_DATA:
-            while not reader.at_eof():
-                r = await reader.read(self.chunksize or -1)
+        elif connection.state['mode'] == ConnectionType.STREAM_DATA:
+            print('READING STREAM')
+            while not connection.reader.at_eof():
+                r = await connection.reader.read(self.chunksize or -1)
                 message = Message(
                     data=r,
                     sender=connection
                     )
                 await self.handleMessage(message)
-                
+        
+        # Should we close conection    
+        if connection.reader.at_eof():
+            print('JA EOF = TRUE')
+            await asyncio.sleep(.1)
+            connection.writer.close()
+            
     async def authenticateConnection(self, connection: Type[Connection]) -> bool:
         '''A template function that should be overwritten by any subclass if required'''
         return True
@@ -136,10 +181,17 @@ class IPCServer(object):
         '''A template function that should be overwritten by any subclass if required'''
         print(message.data.decode())
         
-    async def sendMessage(self, message: Union[str, int, float], connection: Optional[Type[Connection]]=None) -> None:
+    async def sendMessage(self, message: Union[str, int, float], connection: Union[Type[Connection], List[Type[Connection]]]=None) -> None:
+        '''
+        Send a message to either one connection or a list of current connections
+        '''
         if connection:
             connection.writer.write(str(message).encode())
-#             await connection.writer.drain()
+            await connection.writer.drain()
+#             if self.
+#             
+#             Hier überlegen on wir immer schliessen wollen oder icht
+            connection.writer.write_eof()
 #             await asyncio.sleep(.1)
 #             connection.writer.close()
     
@@ -185,7 +237,6 @@ class UXDSocketServer(IPCServer):
                     os.remove(self.path)
                 except Exception as e:
                     raise freedmIPCSocketCreation(f'Cannot delete UXD socket file "{self.path}" ({e})')
-        
         try:
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             sock.bind(self.path)
@@ -194,15 +245,16 @@ class UXDSocketServer(IPCServer):
         
         # Create UDP socket server
         if self.limit:
-            server = await asyncio.start_unix_server(self._onConnection, loop=self.loop, sock=sock, limit=self.limit)
+            server = await asyncio.start_unix_server(self._onConnectionRegister, loop=self.loop, sock=sock, limit=self.limit)
         else:
-            server = await asyncio.start_unix_server(self._onConnection, loop=self.loop, sock=sock)
+            server = await asyncio.start_unix_server(self._onConnectionRegister, loop=self.loop, sock=sock)
         self._context = server
         
         # Return connection class
         return self
 
     async def __aexit__(self, *args) -> None:
+        await super(self.__class__, self).__aexit__(*args)
         print('IPC: We need to stop and cancel the UXD server')
         try:
             self._context.close()
@@ -231,13 +283,27 @@ class UXDSocketServer(IPCServer):
             server_address=None,
             reader=reader,
             writer=writer,
-            mode=self.mode or ConnectionType.TEXT_DATA)
+            state={
+                'mode': self.mode or ConnectionType.TEXT_DATA,
+                'creation': time.time(),
+                'update': time.time() 
+                }
+            )
 
             
 class TCPSocketServer(object):
     '''
     '''
     # https://www.pythonsheets.com/notes/python-socket.html
+    # http://asyncio.readthedocs.io/en/latest/tcp_echo.html
+    
+#     addr = writer.get_extra_info('peername')
+#     print("Received %r from %r" % (message, addr))
+
+    # Mit IP6 oder nicht?
+    # https://www.pythonsheets.com/notes/python-socket.html#simple-tcp-echo-server-through-ipv6
+    # https://www.pythonsheets.com/notes/python-socket.html#disable-ipv6-only
+    # 
             
 
 class freedmIPCSocketCreation(freedmBaseException):
